daa6c98ca651167c5e75fa678665cd2fdac7c87c
Павел <zhilinsky.pa@yandex.ru>
2024-04-25 21:52:09 +0300

    little fix

--- page_analyzer/database.py
+++ page_analyzer/database.py
@@ -19,8 +19,9 @@ def get_all_urls(database):
 def add_url_by_name(database, url):
     conn = psycopg2.connect(database)
     with conn.cursor(cursor_factory=NamedTupleCursor) as curs:
-        curs.execute('''INSERT INTO urls (name, created_at) VALUES (%s, %s) RETURNING id;''',
-                     (url, datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
+        curs.execute(
+            '''INSERT INTO urls (name, created_at) VALUES (%s, %s) RETURNING id;''',
+            (url, datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
         url_info = curs.fetchone()
         id_ = url_info.id
     conn.commit()
